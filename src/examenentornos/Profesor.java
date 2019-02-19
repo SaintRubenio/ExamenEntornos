0ec97248459670a3227cbea08348d1c669119a59
Alumno <Alumno@AULA1-PC04>
2019-02-19 12:13:27 +0100

    Añadir comentarios explicativos para los métodos e inicialización de variables

--- src/examenentornos/Profesor.java
+++ src/examenentornos/Profesor.java
@@ -3,18 +3,25 @@ package examenentornos;
 import java.util.List;
 
 public class Profesor extends Persona{
+    //Declaración variables
     String str;
     int edad;
     String numeroDeTelefono;
     List<Prestamo> prestamos;
+    
+    //Constructor
     public Profesor(String numeroDeTelefono){
         super(numeroDeTelefono);
     }
+    
+    //Obtener información personal: nombre, edad, teléfono
     public void printInformacionPersonal(){
         System.out.println("Nombre: "+str);
         System.out.println("Edad: "+edad);
         System.out.println("Teléfono: "+this.numeroDeTelefono);
     }
+    
+    //Obtener información personal + prestamos
     public void printTodaLaInformacion(){
         System.out.println("Nombre: "+str);
         System.out.println("Edad: "+edad);
