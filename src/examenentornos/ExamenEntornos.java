9cde848bd77cfda428c84fa3e3d9fae06187672c
Alumno <Alumno@AULA1-PC04>
2019-02-19 12:11:22 +0100

    Eliminar comentarios innecesarios

--- src/examenentornos/ExamenEntornos.java
+++ src/examenentornos/ExamenEntornos.java
@@ -1,10 +1,6 @@
 /*Este código define la clase Persona y la clase Profesor
 como una extensión de la primera.
-
-Un profesor es una persona dedicada a la docencia, tanto
-en colegios públicos como en el ámbito privado.
-
-El código ha sido hecho por Carlos Hernández en mayo del 2019*/
+*/
 
 package examenentornos;
 
